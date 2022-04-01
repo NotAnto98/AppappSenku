2f0dd704e1b01df03c5b65af93c911b077c99ff9
NotAnto98 <antoniojesuscarovela@gmail.com>
2022-04-01 18:20:06 +0200

    1

--- app/src/main/java/com/example/appsenku/Tablero.java
+++ app/src/main/java/com/example/appsenku/Tablero.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -30,13 +31,23 @@ public class Tablero extends View {
 //    }
 
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+     /*   super.onDraw(canvas);
         Paint p1 = new Paint(Paint.ANTI_ALIAS_FLAG);
         p1.setColor(Color.RED);
         Bitmap bmp =
                 BitmapFactory.decodeResource(getResources(), R.drawable.board_bg );
-        canvas.drawBitmap(bmp,0,0,p1);
-        canvas.drawCircle();
+        canvas.drawBitmap(bmp,0,0,p1);*/
+
+        for(int y = 0; y < 490*2.5; y+=70*2.5){
+            for(int x = 0; x < 490*2.5; x+=70*2.5){
+                super.onDraw(canvas);
+                Paint p2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+                Bitmap bmp2 =
+                        BitmapFactory.decodeResource(getResources(), R.drawable.vacia);
+                canvas.drawBitmap(bmp2,x,y,p2);
+            }
+        }
+
 
     }
 
