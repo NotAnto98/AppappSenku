8af47cb57210474d45b2db33c151d32c67289b71
antonio <antoniocl11@hotmail.com>
2022-04-06 19:03:27 +0200

    logica movimientos avanzada

--- app/src/main/java/com/example/appsenku/Tablero.java
+++ app/src/main/java/com/example/appsenku/Tablero.java
@@ -1,5 +1,6 @@
 package com.example.appsenku;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,6 +11,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -18,6 +20,10 @@ public class Tablero extends View {
     public static final int VACIA = 0;
     public static final int LIMITE = -1;
     public static final int FICHA = 1;
+    public int selX = 0;
+    public int selY = 0;
+    public Boolean selected = false;
+    public Activity test = null;
 
     public Tablero(Context context) {
         super(context);
@@ -55,26 +61,89 @@ public class Tablero extends View {
         int g = y-1;
         int h = y-2;
 
-        if(a < 7 && b < 7){
-            if(this.matriz[x][y] == 1 && this.matriz[a][y] == 1 && this.matriz[b][y] == 0){
-                Log.d("Senku", "derecha");
-            }
-        }
-        if(c >= 0 && d >= 0){
-            if(this.matriz[x][y] == 1 && this.matriz[c][y] == 1 && this.matriz[d][y] == 0){
+        if(selected){
+            if(this.matriz[x][y] == 0 && this.matriz[c][y] == 1 && d == selX && y == selY){
                 Log.d("Senku", "izquierda");
+                selX = x;
+                selY = y;
+                this.matriz[x][y] = 1;
+                this.matriz[c][y] = 0;
+                this.matriz[d][y] = 0;
+                selected = false;
+                this.invalidate();
             }
-        }
-        if(e < 7 && f < 7){
-            if(this.matriz[x][y] == 1 && this.matriz[x][e] == 1 && this.matriz[x][f] == 0){
-                Log.d("Senku", "arriba");
+            if(this.matriz[x][y] == 0 && this.matriz[a][y] == 1 && b == selX && y == selY){
+                Log.d("Senku", "derecha");
+                selX = x;
+                selY = y;
+                this.matriz[x][y] = 1;
+                this.matriz[a][y] = 0;
+                this.matriz[b][y] = 0;
+                selected = false;
+                this.invalidate();
             }
-        }
-        if(g >= 0 && h >= 0){
-            if(this.matriz[x][y] == 1 && this.matriz[x][g] == 1 && this.matriz[x][h] == 0){
+            if(this.matriz[x][y] == 0 && this.matriz[x][e] == 1 && x == selX && f == selY){
                 Log.d("Senku", "abajo");
+                selX = x;
+                selY = y;
+                this.matriz[x][y] = 1;
+                this.matriz[x][e] = 0;
+                this.matriz[x][f] = 0;
+                selected = false;
+                this.invalidate();
+            }
+            if(this.matriz[x][y] == 0 && this.matriz[x][g] == 1 && x == selX && h == selY){
+                Log.d("Senku", "arriba");
+                selX = x;
+                selY = y;
+                this.matriz[x][y] = 1;
+                this.matriz[x][g] = 0;
+                this.matriz[x][h] = 0;
+                selected = false;
+                this.invalidate();
             }
         }
+        else{
+            if(a < 7 && b < 7){
+                if(this.matriz[x][y] == 1 && this.matriz[a][y] == 1 && this.matriz[b][y] == 0){
+                    Log.d("Senku", "derecha");
+                    selX = x;
+                    selY = y;
+                    selected = true;
+                    Toast toast = Toast.makeText(test, "Ajustes guardados", Toast.LENGTH_LONG);
+                    toast.show();
+                    this.invalidate();
+                }
+            }
+            if(c >= 0 && d >= 0){
+                if(this.matriz[x][y] == 1 && this.matriz[c][y] == 1 && this.matriz[d][y] == 0){
+                    Log.d("Senku", "izquierda");
+                    selX = x;
+                    selY = y;
+                    selected = true;
+                    this.invalidate();
+                }
+            }
+            if(e < 7 && f < 7){
+                if(this.matriz[x][y] == 1 && this.matriz[x][e] == 1 && this.matriz[x][f] == 0){
+                    Log.d("Senku", "abajo");
+                    selX = x;
+                    selY = y;
+                    selected = true;
+                    this.invalidate();
+                }
+            }
+            if(g >= 0 && h >= 0){
+                if(this.matriz[x][y] == 1 && this.matriz[x][g] == 1 && this.matriz[x][h] == 0){
+                    Log.d("Senku", "arriba");
+                    selX = x;
+                    selY = y;
+                    selected = true;
+                    this.invalidate();
+                }
+            }
+        }
+
 
 
         //Log.d("SENKU","X="+x);
@@ -107,6 +176,12 @@ public class Tablero extends View {
                     canvas.drawBitmap(bmp2,(int)i,(int)j,p2);
                 }
             }
+        }
+
+        if(selected == true){
+            Paint p1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+            p1.setColor(Color.RED);
+            canvas.drawRect(selX*70*2.5f, selY*70*2.5f, (selX+1)*70*2.5f-10, (selY+1)*70*2.5f-10, p1);
         }
     }
 
