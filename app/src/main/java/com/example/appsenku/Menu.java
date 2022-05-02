c9b9fc24684ae4d5a0f716ec57b6792df49d9d5a
antonio <antoniocl11@hotmail.com>
2022-05-02 17:10:58 +0200

    menu actualizado y más codigo sobre la logica del juego, ahora comprueba cuando la partida se pierde

--- app/src/main/java/com/example/appsenku/Menu.java
+++ app/src/main/java/com/example/appsenku/Menu.java
@@ -1,5 +1,45 @@
 package com.example.appsenku;
 
-public class Menu {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Menu extends AppCompatActivity {
+
+    public Activity test = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button info = findViewById(R.id.jugar);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivity(intent);
+                //sería interesante poner que cuando pulses en jugar salga un toast o algun mensaje
+                //explicando que hace cada boton en el menu de juego
+            }
+        });
+
+        Button cerrar = findViewById(R.id.salir);
+        cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Crear un llamado a la clase principal pero
+                //en el momento que se llama se borren todas las actividades
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/appsenku/Tablero.java
+++ app/src/main/java/com/example/appsenku/Tablero.java
@@ -15,7 +15,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tablero extends View {
 
@@ -25,7 +26,6 @@ public class Tablero extends View {
     public int selX = 0;
     public int selY = 0;
     public Boolean selected = false;
-    public static Boolean resetV = false;
     public Activity test = null;
 
     public Tablero(Context context) {
@@ -40,51 +40,22 @@ public class Tablero extends View {
         super(context, attrs, defStyleAttr);
     }
 
-    public int[][] reset_matriz(){
-        return new int[][] {{-1,-1,1,1,1,-1,-1},
-                {-1,-1,1,1,1,-1,-1},
-                {1,1,1,1,1,1,1},
-                {1,1,1,0,1,1,1},
-                {1,1,1,1,1,1,1},
-                {-1,-1,1,1,1,-1,-1},
-                {-1,-1,1,1,1,-1,-1}};
-    }
-
-
-
-    public static int[][] matriz=  new int[][]{
-        {-1,-1,1,1,1,-1,-1},
-        {-1,-1,1,1,1,-1,-1},
-        {1,1,1,1,1,1,1},
-        {1,1,1,0,1,1,1},
-        {1,1,1,1,1,1,1},
-        {-1,-1,1,1,1,-1,-1},
-        {-1,-1,1,1,1,-1,-1}
-
+    public int[][] matriz=  new int[][]{
+            {-1,-1,1,1,1,-1,-1},
+            {-1,-1,1,1,1,-1,-1},
+            {1,1,1,1,1,1,1},
+            {1,1,1,0,1,1,1},
+            {1,1,1,1,1,1,1},
+            {-1,-1,1,1,1,-1,-1},
+            {-1,-1,1,1,1,-1,-1}
     };
-
-    public void full_reset(){
-
-        matriz = new int[][]{
-                {-1,-1,1,1,1,-1,-1},
-                {-1,-1,1,1,1,-1,-1},
-                {1,1,1,1,1,1,1},
-                {1,1,1,0,1,1,1},
-                {1,1,1,1,1,1,1},
-                {-1,-1,1,1,1,-1,-1},
-                {-1,-1,1,1,1,-1,-1}
-
-        };
-        //matriz = reset_matriz();
-
-    }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-
         int x= (int)(event.getX()/(70*2.5));
         int y = (int)(event.getY()/(70*2.5));
 
+        boolean compruebaFinal = true;
 
         int a = x+1;
         int b = x+2;
@@ -95,109 +66,230 @@ public class Tablero extends View {
         int g = y-1;
         int h = y-2;
 
-
         if(selected){
-            if(this.matriz[x][y] == 0 && this.matriz[c][y] == 1 && d == selX && y == selY){
-                Log.d("Senku", "izquierda");
+
+            if(matriz[x][y]==1){ //seleccion de ficha
+                selX = x;
+                selY = y;
+                this.invalidate();
+            }
+            if(c >=0 && d >=0 && this.matriz[x][y] == 0 && this.matriz[c][y] == 1 && d == selX && y == selY){ //movimiento legal hacia la izquierda
+
                 selX = x;
                 selY = y;
                 this.matriz[x][y] = 1;
                 this.matriz[c][y] = 0;
                 this.matriz[d][y] = 0;
                 selected = false;
+                compruebaFinal();
                 this.invalidate();
             }
-            if(this.matriz[x][y] == 0 && this.matriz[a][y] == 1 && b == selX && y == selY){
-                Log.d("Senku", "derecha");
+            if(a < 7 && b < 7 && this.matriz[x][y] == 0 && this.matriz[a][y] == 1 && b == selX && y == selY){ //movimiento legal hacia la derecha
+
                 selX = x;
                 selY = y;
                 this.matriz[x][y] = 1;
                 this.matriz[a][y] = 0;
                 this.matriz[b][y] = 0;
                 selected = false;
+                compruebaFinal();
                 this.invalidate();
             }
-            if(this.matriz[x][y] == 0 && this.matriz[x][e] == 1 && x == selX && f == selY){
-                Log.d("Senku", "abajo");
+            if(e < 7 && f < 7 && this.matriz[x][y] == 0 && this.matriz[x][e] == 1 && x == selX && f == selY){ //movimiento legal hacia abajo
                 selX = x;
                 selY = y;
                 this.matriz[x][y] = 1;
                 this.matriz[x][e] = 0;
                 this.matriz[x][f] = 0;
                 selected = false;
+                compruebaFinal();
                 this.invalidate();
             }
-            if(this.matriz[x][y] == 0 && this.matriz[x][g] == 1 && x == selX && h == selY){
-                Log.d("Senku", "arriba");
+            if(h >=0 && g >=0 && this.matriz[x][y] == 0 && this.matriz[x][g] == 1 && x == selX && h == selY){ //movimiento legal hacia arriba
                 selX = x;
                 selY = y;
                 this.matriz[x][y] = 1;
                 this.matriz[x][g] = 0;
                 this.matriz[x][h] = 0;
                 selected = false;
+                compruebaFinal();
                 this.invalidate();
+            }
+
+            else{ //movimiento ilegal
+                if(matriz[x][y]==0){
+                    Toast toast = Toast.makeText(test, "Movimiento no válido", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         }
         else{
-            if(a < 7 && b < 7){
-                if(this.matriz[x][y] == 1 && this.matriz[a][y] == 1 && this.matriz[b][y] == 0){
-                    Log.d("Senku", "derecha");
-                    selX = x;
-                    selY = y;
-                    selected = true;
-                    Toast toast = Toast.makeText(test, "Ajustes guardados", Toast.LENGTH_LONG);
-                    toast.show();
-                    this.invalidate();
-                }
+            if(matriz[x][y]==1){ //seleccion de ficha.
+                selX = x;
+                selY = y;
+                selected = true;
+                this.invalidate();
             }
-            if(c >= 0 && d >= 0){
-                if(this.matriz[x][y] == 1 && this.matriz[c][y] == 1 && this.matriz[d][y] == 0){
-                    Log.d("Senku", "izquierda");
-                    selX = x;
-                    selY = y;
-                    selected = true;
-                    this.invalidate();
-                }
+
+            if(matriz[x][y]==0){ //movimineto ilegal, hueco vacio
+                Toast toast = Toast.makeText(test, "Movimiento no válido", Toast.LENGTH_SHORT);
+                toast.show();
             }
-            if(e < 7 && f < 7){
-                if(this.matriz[x][y] == 1 && this.matriz[x][e] == 1 && this.matriz[x][f] == 0){
-                    Log.d("Senku", "abajo");
-                    selX = x;
-                    selY = y;
-                    selected = true;
-                    this.invalidate();
+
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    //metodo manejador botón CLUE
+    public List<List<Integer>> movimientosPista(){
+
+        List<List<Integer>> movimientosPosibles = new ArrayList<>();
+
+        for(int y1 = 0; y1< matriz.length; y1++){
+            for(int x1 = 0; x1< matriz.length; x1++){
+                //movimiento posible para la izquierda
+                if(x1-1 >=0 && x1-2 >=0 && this.matriz[x1][y1] == 1 && this.matriz[x1-1][y1] == 1 && this.matriz[x1-2][y1] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+
                 }
-            }
-            if(g >= 0 && h >= 0){
-                if(this.matriz[x][y] == 1 && this.matriz[x][g] == 1 && this.matriz[x][h] == 0){
-                    Log.d("Senku", "arriba");
-                    selX = x;
-                    selY = y;
-                    selected = true;
-                    this.invalidate();
+                //movimiento posible para la derecha
+                if(x1+1 < 7 && x1+2 < 7 && this.matriz[x1][y1] == 1 && this.matriz[x1+1][y1] == 1 && this.matriz[x1+2][y1] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+                }
+                //movimiento posible hacia arriba
+                if(y1-1 >=0 && y1-2 >=0 && this.matriz[x1][y1] == 1 && this.matriz[x1][y1-1] == 1 && this.matriz[x1][y1-2] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+                }
+                //movimiento posible hacia abajo
+                if(y1+1 <7 && y1+2 <7 && this.matriz[x1][y1] == 1 && this.matriz[x1][y1+1] == 1 && this.matriz[x1][y1+2] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
                 }
             }
         }
 
+        int n = (int)Math.round(Math.random() * (movimientosPosibles.size()));
+        int posible1 = movimientosPosibles.get(n).get(0);
+        int posible2 = movimientosPosibles.get(n).get(1);
+        selected = true;
+        selX = posible1;
+        selY = posible2;
+        this.invalidate();
 
-
-        //Log.d("SENKU","X="+x);
-        //Log.d("SENKU","Y="+y);
-        //estadoMovimiento(event);
-        return super.dispatchTouchEvent(event);
+        return movimientosPosibles;
     }
 
-    protected void onDraw(Canvas canvas) {
+    //método partida ganada, consiste en comprobar que quede una única ficha
+    //si esto es asi la partida ha sido ganada
+    public boolean partidaGanada(){
 
+        boolean res = true;
+        int cont = 0;
+
+        for(int y1 = 0; y1< matriz.length; y1++){
+            for(int x1 = 0; x1< matriz.length; x1++){
+                if(cont > 1) {
+                    res = false;
+                    break;
+                }
+                else if(this.matriz[x1][y1]==1){
+                    cont++;
+                }
+
+            }
+        }
+
+        return res;
+    }
+
+
+    public boolean gameOver(){
+
+        List<List<Integer>> movimientosPosibles = new ArrayList<>();
+
+        for(int y1 = 0; y1< matriz.length; y1++){
+            for(int x1 = 0; x1< matriz.length; x1++){
+                //movimiento posible para la izquierda
+                if(x1-1 >=0 && x1-2 >=0 && this.matriz[x1][y1] == 1 && this.matriz[x1-1][y1] == 1 && this.matriz[x1-2][y1] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+
+                }
+                //movimiento posible para la derecha
+                if(x1+1 < 7 && x1+2 < 7 && this.matriz[x1][y1] == 1 && this.matriz[x1+1][y1] == 1 && this.matriz[x1+2][y1] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+                }
+                //movimiento posible hacia arriba
+                if(y1-1 >=0 && y1-2 >=0 && this.matriz[x1][y1] == 1 && this.matriz[x1][y1-1] == 1 && this.matriz[x1][y1-2] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+                }
+                //movimiento posible hacia abajo
+                if(y1+1 <7 && y1+2 <7 && this.matriz[x1][y1] == 1 && this.matriz[x1][y1+1] == 1 && this.matriz[x1][y1+2] == 0){
+                    List<Integer> posiciones = new ArrayList<>();
+                    posiciones.add(x1);
+                    posiciones.add(y1);
+                    movimientosPosibles.add(posiciones);
+                }
+            }
+        }
+
+        Log.d("mov posible", "mov posibles " + movimientosPosibles);
+
+        if(movimientosPosibles.size() == 0){
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
+
+    public boolean compruebaFinal(){
+        boolean res = true;
+        if(partidaGanada() == true){
+            Toast toast = Toast.makeText(test, "Partida Ganada", Toast.LENGTH_SHORT);
+            toast.show();
+            res = false;
+            Log.d("ganada", "ganada");
+        }
+
+        else if(gameOver() == true){
+            Toast toast = Toast.makeText(test, "Partida perdida", Toast.LENGTH_SHORT);
+            toast.show();
+            res = false;
+            Log.d("perdida", "perdida");
+        }
+
+        return res;
+    }
+
+
+    protected void onDraw(Canvas canvas) {
 
         for(int y = 0; y<7; y++){
             for(int x = 0; x<7; x++){
                 double i = x*70*2.5;
                 double j = y*70*2.5;
-                if(matriz[x][y] == -1){
-
-                }
-                else if(matriz[x][y] == 0){
+                if(matriz[x][y] == 0){
                     super.onDraw(canvas);
                     Paint p2 = new Paint(Paint.ANTI_ALIAS_FLAG);
                     Bitmap bmp2 =
@@ -217,31 +309,14 @@ public class Tablero extends View {
         if(selected == true){
             Paint p1 = new Paint(Paint.ANTI_ALIAS_FLAG);
             p1.setColor(Color.RED);
-            canvas.drawRect(selX*70*2.5f, selY*70*2.5f, (selX+1)*70*2.5f-10, (selY+1)*70*2.5f-10, p1);
+            Bitmap bmp3 = BitmapFactory.decodeResource(getResources(), R.drawable.ficha_seleccionada);
+            canvas.drawBitmap(bmp3,selX*70*2.5f,selY*70*2.5f,p1);
         }
-    }
 
-    public boolean estadoMovimiento(MotionEvent event){
-        int x= (int)(event.getX()/(70*2.5));
-        int y = (int)(event.getY()/(70*2.5));
-        boolean movPosible = false;
-        for( ;y<5; y++){
-            for(;x<5;x++){
-                if((this.matriz[x][y] == 1 && this.matriz[x+1][y] == 1 && this.matriz[x+2][y] == 0) ||
-                        (this.matriz[x][y] == 0 && this.matriz[x+1][y] == 1 && this.matriz[x+2][y] == 1) ||
-                        (this.matriz[x][y] == 1 && this.matriz[x][y+1] == 1 && this.matriz[x][y+2] == 0) ||
-                        (this.matriz[x][y] == 0 && this.matriz[x][y+1] == 1 && this.matriz[x][y+2] == 1)){
-                    Log.d("SENKU","se pueden hacer movimientos"+x);
-                    movPosible = true;
-                }else{
-                    Log.d("SENKU","no se pueden hacer movimientos");
-                }
 
-            }
-        }
-        return movPosible;
     }
 
 
 
 }
+
